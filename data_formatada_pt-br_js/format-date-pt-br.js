fb262854048fd5ef74e634a302d5e28321f646ef
Victor Eduardo <convictor.developer@gmail.com>
2025-02-24 20:00:38 -0300

    Script Formatar data em pt-BR.

--- data_formatada_pt-br_js/format-date-pt-br.js
+++ data_formatada_pt-br_js/format-date-pt-br.js
@@ -1,12 +1,12 @@
 const date = new Date();
-
+// Função para formatar date em pt-br.
 function dateFormat(date){
     const day = addZero(date.getDate());
     const month = addZero(date.getMonth());
     const year = addZero(date.getFullYear());
     return `${day}/${month}/${year}`;
 }
-
+// Função para adicionar zero em números menores do que 10.
 function addZero(num){
     return num >= 10 ? num : `0${num}`;
 }
